7d64392b643b2be926612f5bad6b7e432299fa4c
Abiyasa Hendro <abiyasa.hendro@dailyact.com>
2020-09-11 14:16:08 +0700

    episode 06

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text("this one though"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("this title though"),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            "this centered body though omg",
+            style: TextStyle(
+              fontFamily: "IndieFlower",
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Text("+"),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
     ));
